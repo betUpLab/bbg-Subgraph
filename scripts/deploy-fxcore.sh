f0fd4871fe0f7ec3fa76c6c21a2d4942ea10f821
MJ <36900146@qq.com>
2024-05-11 11:39:41 +0800

    config

---
+++ scripts/deploy-fxcore.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -o errexit -o pipefail
+
+subgraphName="subgraphBBG"
+graphNodePubUrl="https://testnet-graph-node.functionx.io"
+graphNodeUrl="${TESTNET_GRAPH_NODE_URL}"
+ipfsUrl="${TESTNET_GRAPH_IPFS_URL}"
+
+yarn
+yarn codegen
+npx graph build
+
+echo "Deploying to Graph node"
+npx graph create --node "${graphNodeUrl}" "${subgraphName}"
+npx graph deploy --ipfs "${ipfsUrl}" --node "${graphNodeUrl}" "${subgraphName}"
+echo "Deployed to $graphNodePubUrl/subgraphs/name/${subgraphName}/graphql"
+echo "Subgraph endpoints:"
+echo "Queries (HTTP): $graphNodePubUrl/subgraphs/name/${subgraphName}"
+echo "Subscriptions (WS): $graphNodePubUrl/ws/subgraphs/name/${subgraphName}"
