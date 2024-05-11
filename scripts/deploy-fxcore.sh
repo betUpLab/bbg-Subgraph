1f3a475f9e97c4011e9b40d444d7f12166c32643
MJ <36900146@qq.com>
2024-05-11 17:52:46 +0800

    config

--- scripts/deploy-fxcore.sh
+++ scripts/deploy-fxcore.sh
@@ -13,23 +13,10 @@ npx graph build
 
 # echo "Deploying to Graph node"
 npx graph create --node "${graphNodeUrl}" "${subgraphName}"
-# npx graph deploy --ipfs "${ipfsUrl}" --node "${graphNodeUrl}" "${subgraphName}"
-# npx graph deploy --ipfs "${ipfsUrl}" --node "${graphNodeUrl}" $name ${subgraph} -l "v0.0.1"
-# npx graph deploy --ipfs "${ipfsUrl}" --node "${graphNodeUrl}" $name ${subgraph}subgraph.yaml -l "v0.0.1"
 
+npx graph deploy --ipfs "${ipfsUrl}" --node "${graphNodeUrl}" $subgraphName subgraph.yaml -l "v0.0.1"
 
-echo "Deploying subgraph ${subgraphName} to Graph node"
-    if npx graph deploy --ipfs "${ipfsUrl}" --node "${graphNodeUrl}" "${subgraphName}" >/dev/null 2>&1; then
-      echo "Deploying to Graph node"
-      echo "Deployed to $graphNodePubUrl/subgraphs/name/${subgraphName}/graphql"
-      echo "Subgraph endpoints:"
-      echo "Queries (HTTP): $graphNodePubUrl/subgraphs/name/${subgraphName}"
-      echo "Subscriptions (WS): $graphNodePubUrl/ws/subgraphs/name/${subgraphName}"
-    else
-      echo "Failed to deploy ${subgraphName}!"
-    fi
-
-# echo "Deployed to $graphNodePubUrl/subgraphs/name/${subgraphName}/graphql"
-# echo "Subgraph endpoints:"
-# echo "Queries (HTTP): $graphNodePubUrl/subgraphs/name/${subgraphName}"
-# echo "Subscriptions (WS): $graphNodePubUrl/ws/subgraphs/name/${subgraphName}"
+echo "Deployed to $graphNodePubUrl/subgraphs/name/${subgraphName}/graphql"
+echo "Subgraph endpoints:"
+echo "Queries (HTTP): $graphNodePubUrl/subgraphs/name/${subgraphName}"
+echo "Subscriptions (WS): $graphNodePubUrl/ws/subgraphs/name/${subgraphName}"
