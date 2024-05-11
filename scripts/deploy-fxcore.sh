f3084fbd3f6db5787b02057cf005668fce46a6d7
MJ <36900146@qq.com>
2024-05-11 17:34:33 +0800

    config

--- scripts/deploy-fxcore.sh
+++ scripts/deploy-fxcore.sh
@@ -11,11 +11,25 @@ yarn
 yarn codegen
 npx graph build
 
-echo "Deploying to Graph node"
+# echo "Deploying to Graph node"
 npx graph create --node "${graphNodeUrl}" "${subgraphName}"
 # npx graph deploy --ipfs "${ipfsUrl}" --node "${graphNodeUrl}" "${subgraphName}"
-npx graph deploy --ipfs "${ipfsUrl}" --node "${graphNodeUrl}" $name ${subgraph} -l "v0.0.1"
-echo "Deployed to $graphNodePubUrl/subgraphs/name/${subgraphName}/graphql"
-echo "Subgraph endpoints:"
-echo "Queries (HTTP): $graphNodePubUrl/subgraphs/name/${subgraphName}"
-echo "Subscriptions (WS): $graphNodePubUrl/ws/subgraphs/name/${subgraphName}"
+# npx graph deploy --ipfs "${ipfsUrl}" --node "${graphNodeUrl}" $name ${subgraph} -l "v0.0.1"
+# npx graph deploy --ipfs "${ipfsUrl}" --node "${graphNodeUrl}" $name ${subgraph}subgraph.yaml -l "v0.0.1"
+
+
+echo "Deploying subgraph ${subgraphName} to Graph node"
+    if npx graph deploy --ipfs "${ipfsUrl}" --node "${graphNodeUrl}" "${subgraphName}" >/dev/null 2>&1; then
+      echo "Deploying to Graph node"
+      echo "Deployed to $graphNodePubUrl/subgraphs/name/${subgraphName}/graphql"
+      echo "Subgraph endpoints:"
+      echo "Queries (HTTP): $graphNodePubUrl/subgraphs/name/${subgraphName}"
+      echo "Subscriptions (WS): $graphNodePubUrl/ws/subgraphs/name/${subgraphName}"
+    else
+      echo "Failed to deploy ${subgraphName}!"
+    fi
+
+# echo "Deployed to $graphNodePubUrl/subgraphs/name/${subgraphName}/graphql"
+# echo "Subgraph endpoints:"
+# echo "Queries (HTTP): $graphNodePubUrl/subgraphs/name/${subgraphName}"
+# echo "Subscriptions (WS): $graphNodePubUrl/ws/subgraphs/name/${subgraphName}"
