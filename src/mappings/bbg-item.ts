5e64145ff8ae97e171162763a6a8f2d235abfede
MJ <36900146@qq.com>
2024-06-25 11:59:15 +0800

    fix

--- src/mappings/bbg-item.ts
+++ src/mappings/bbg-item.ts
@@ -1,4 +1,3 @@
-import { Bytes, ethereum } from "@graphprotocol/graph-ts"
 import {
     TransferSingle as TransferEvent,
   } from "../../generated/BBGItem/BBGItem"
@@ -7,24 +6,21 @@ import {
     TransferSingle
   } from "../../generated/schema"
 import { bbgItemContract } from "./helpers"
+
+
   
 export function handleTransferSingle(event: TransferEvent): void {
+    // let entity = new TransferSingle(  
+    //   event.transaction.hash.concatI32(event.logIndex.toI32())
+
+    // )
     let entity = new TransferSingle(
-      event.transaction.hash.toHexString().concat(event.logIndex.toI32().toString())
+      event.transaction.hash.toHex() + "-" + event.logIndex.toString()
     )
     entity.from = event.params.from
     entity.to = event.params.to
     entity.amount = event.params.value
-    entity.id = event.params.id.toString()
-
-
-      // let items =  bbgItemContract.try_itemDetail(Bytes.fromHexString(entity.id));
-
-      // if (!items.reverted) {
-      //   entity.name = items.value.getName();
-      // } else {
-      //   entity.name = "unknown";
-      // }
+    entity.itemID = event.params.id.toString()
 
     entity.operator = event.params.operator
     entity.blockNumber = event.block.number
